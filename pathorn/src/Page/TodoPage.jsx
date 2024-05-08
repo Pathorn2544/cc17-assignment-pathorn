2a9835f6763c19753a407daaebcecc58cfe321a1
Pathorn2544 <pathorn2001@gmai.com>
2024-05-08 18:11:14 +0700

    csss

--- pathorn/src/Page/TodoPage.jsx
+++ pathorn/src/Page/TodoPage.jsx
@@ -9,7 +9,6 @@ function TodoPage(){
     
    return(
 
-
     <div className="work">
         <header>
             <h1>My Todo</h1>
@@ -17,7 +16,7 @@ function TodoPage(){
         </header>
         <div className="list">
 
-            <div> </div>
+            
             <button onClick={deleate}>x</button>
 
         </div>
